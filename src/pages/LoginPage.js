06648507688c49fb596d69e488203191ef002c48
Bartłomiej Janota <bartlomiej.janota@gmail.com>
2025-03-03 00:15:32 +0100

    MAT-34 Połączenie frontendu i backendu. (Logowanie i rejestracja)

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,31 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Bacground from "../features/Bacground/Bacground";
 
-
-
 const LoginPage = () => {
   const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [accessToken, setAccessToken] = useState("");
 
-  const handleLogin = (e) => {
+  const isValidPassword = (password) => {
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+  };
+
+  const handleLogin = async (e) => {
     e.preventDefault();
-    navigate("/menu");
+
+    if (username.length < 8) {
+      setErrorMessage("Nazwa użytkownika musi mieć co najmniej 8 znaków.");
+      return;
+    }
+
+    if (!isValidPassword(password)) {
+      setErrorMessage(
+        "Hasło musi mieć min. 8 znaków, 1 małą literę, 1 dużą literę, 1 cyfrę i 1 znak specjalny."
+      );
+      return;
+    }
+
+    try {
+      const response = await fetch(`/User/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setAccessToken(data.accessToken);
+        console.log("Access Token:", data.accessToken);
+        setErrorMessage("");
+        navigate("/menu");
+      } else {
+        const errorData = await response.json();
+        setErrorMessage(errorData.detail || "Błąd logowania.");
+      }
+    } catch (error) {
+      setErrorMessage("Błąd połączenia z serwerem.");
+    }
   };
 
   return (
-  
     <div className="auth-container">
-      <Bacground/>
-      <div class="contour f-right-top"></div>
-      <div class="contour f-right-bottom"></div>
-      <div class="contour s-right-top"></div>
-      <div class="contour s-right-bottom"></div>
-      <div class="contour f-left-top"></div>
-      <div class="contour f-left-bottom"></div>
-      <div class="contour s-left-top"></div>
-      <div class="contour s-left-bottom"></div>
-
-
+      <Bacground />
       <h1 className="app-title">SpaceMath</h1>
       <div className="auth-box">
         <form onSubmit={handleLogin}>
@@ -33,18 +64,25 @@ const LoginPage = () => {
           <input
             type="text"
             className="auth-input"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             required
           />
+
           <p className="auth-words">Hasło</p>
           <input
             type="password"
             className="auth-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             required
           />
-          <button type="submit" className="auth-button">
-            Zaloguj się
-          </button>
+
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+          <button type="submit" className="auth-button">Zaloguj się</button>
         </form>
+
         <div className="auth-links">
           <a href="/forgot-password">Zapomniałeś hasła?</a>
           <a href="/register">Zarejestruj się</a>
